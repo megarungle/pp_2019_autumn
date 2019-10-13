ee8a7a8d75b1810113d996de3f323937267588f0
megarungle <megarungle@gmail.com>
2019-10-13 21:21:04 +0300

    Fixed code style (2)

--- modules/task_1/utkin_k_num_of_alternations/num_of_alternations.h
+++ modules/task_1/utkin_k_num_of_alternations/num_of_alternations.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Utkin Konstantin
-#ifndef MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS_
-#define MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS_
+#ifndef MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS_NUM_OF_ALTERNATIONS_H_
+#define MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS_NUM_OF_ALTERNATIONS_H_
 
 #include <vector>
 #include <string>
@@ -10,4 +10,4 @@ std::vector<int> getRandomVector(int size);
 int getSequentialOperations(std::vector<int> vec);
 int getParallelOperations(std::vector<int> globalVec);
 
-#endif  // MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS_
+#endif  // MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS_NUM_OF_ALTERNATIONS_H_
