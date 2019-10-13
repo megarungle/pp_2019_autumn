df602ef124186716a3c0333d68580df3b8c9a995
megarungle <megarungle@gmail.com>
2019-10-13 17:25:29 +0300

    Implementation of h and cpp

---
+++ modules/task_1/utkin_k_num_of_alternations/num_of_alternations.h
@@ -0,0 +1,13 @@
+// Copyright 2019 Utkin Konstantin
+#ifndef MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS
+#define MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS
+
+#include <vector>
+#include <string>
+
+std::vector<int> getRandomVector(int size);
+
+int getSequentialOperations(std::vector<int> vec);
+int getParallelOperations(std::vector<int> globalVec);
+
+#endif // MODULES_TASK_1_UTKIN_K_NUM_OF_ALTERNATIONS
