3f28540adf0ff6339003ac932ed23bd127780ce0
megarungle <megarungle@gmail.com>
2019-11-15 05:16:28 +0300

    parallel sorting implementation

---
+++ modules/task_2/utkin_k_odd_even_sort/main.cpp
@@ -0,0 +1,20 @@
+// Copyright 2019 Utkin Konstantin
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <vector>
+#include "./odd_even_sort.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
