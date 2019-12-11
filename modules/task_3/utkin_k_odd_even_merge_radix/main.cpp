b1de925386203977cf4d3c8e71d0924782acfffd
megarungle <megarungle@gmail.com>
2019-12-11 18:34:11 +0300

    initial commit

---
+++ modules/task_3/utkin_k_odd_even_merge_radix/main.cpp
@@ -0,0 +1,21 @@
+// Copyright 2019 Utkin Konstantin
+#include <gtest-mpi-listener.hpp>
+#include <gtest/gtest.h>
+#include <algorithm>
+#include <vector>
+#include "./odd_even_merge_radix.h"
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
+
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
+
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
+
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
+}
---
+++ modules/task_3/utkin_k_odd_even_merge_radix/odd_even_merge_radix.cpp
@@ -0,0 +1,6 @@
+// Copyright 2019 Utkin Konstantin
+#include <mpi.h>
+#include <random>
+#include <vector>
+#include <ctime>
+#include "../../../modules/task_3/utkin_k_odd_even_merge_radix/odd_even_merge_radix.h"
