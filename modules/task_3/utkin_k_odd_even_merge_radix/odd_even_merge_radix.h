fc3632087e3fd57ad455d920d4192e0ced930519
megarungle <megarungle@gmail.com>
2019-12-12 20:50:23 +0300

    fix (2)

--- modules/task_3/utkin_k_odd_even_merge_radix/odd_even_merge_radix.h
+++ modules/task_3/utkin_k_odd_even_merge_radix/odd_even_merge_radix.h
@@ -1,6 +1,6 @@
 // Copyright 2019 Utkin Konstantin
-#ifndef MODULES_TASK_3_UTKIN_K_ODD_EVEN_MERGE_RADIX_ODD_EVEN_MERGE_RADIX_H
-#define MODULES_TASK_3_UTKIN_K_ODD_EVEN_MERGE_RADIX_ODD_EVEN_MERGE_RADIX_H
+#ifndef MODULES_TASK_3_UTKIN_K_ODD_EVEN_MERGE_RADIX_ODD_EVEN_MERGE_RADIX_H_
+#define MODULES_TASK_3_UTKIN_K_ODD_EVEN_MERGE_RADIX_ODD_EVEN_MERGE_RADIX_H_
 
 #include <vector>
 
@@ -10,4 +10,4 @@ std::vector<double> sortingByCounting(std::vector<double> vec1,
     std::vector<double> vec2, int byte);
 std::vector<double> radixSort(std::vector<double> vec);
 
-#endif  // MODULES_TASK_3_UTKIN_K_ODD_EVEN_MERGE_RADIX_ODD_EVEN_MERGE_RADIX_H
+#endif  // MODULES_TASK_3_UTKIN_K_ODD_EVEN_MERGE_RADIX_ODD_EVEN_MERGE_RADIX_H_
